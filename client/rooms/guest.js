baaf16bb015fe62c97084a4b8927849128ed14e9
Pedro Henrique <pedrohenriquerls@gmail.com>
2014-09-12 22:23:54 -0300

    filter for guest display

--- client/rooms/guest.js
+++ client/rooms/guest.js
@@ -8,10 +8,14 @@ Template.room_guest.created = function(){
 			  Template.room_guest.guestCTX.drawImage(image, 0, 0);
 			};
 		}else{
-			var guestCanvas = document.getElementById("guest_canvas")
+			var guestCanvas = document.createElement("canvas")
 		  Template.room_guest.guestCTX = guestCanvas.getContext("2d");
 			guestCanvas.width=256
 			guestCanvas.height=224
+
+			var renderFilter = require("render")			
+			renderFilter.init(false, guestCanvas, "guestDisplay")
+			renderFilter.animate()
 		}
 	});
 }
